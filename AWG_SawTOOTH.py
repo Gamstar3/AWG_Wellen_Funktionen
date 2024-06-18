fbafd4a2a16d8dd3fbbed6b6b506427fa5b25039
Gamstar3 <149072035+Gamstar3@users.noreply.github.com>
2024-06-18 13:21:26 +0200

    AWG_SawTOOTH.py

--- AWG_SawTOOTH.py
+++ AWG_SawTOOTH.py
@@ -1,11 +1,6 @@
 import numpy as np
 
-f_1 = 1.0   #Grundfrequenz
-T = 10.0     #Zeitraum
-N = 20000     #Anzahl der Summenglieder
-S_R = 1000   #Abtastrate
-
-def sawtooth_wave(f_1, N, S_R, T):      #Sägezahn
+def sawtooth_wave(f_1, N, S_R, T):     
     t = np.linspace(0, T, int(T * S_R))
     signal = np.zeros_like(t)
     for n in range(1, N+1):
