c63ba09e2c21f24c5a8af143a06e4c9d55a82901
Gamstar3 <149072035+Gamstar3@users.noreply.github.com>
2024-06-18 13:24:20 +0200

    Add files via upload

---
+++ AWG_SawTOOTH.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def sawtooth_wave(f_1, N, S_R, T):      
+    t = np.linspace(0, T, int(T * S_R))
+    signal = np.zeros_like(t)
+    for n in range(1, N+1):
+        signal += (1/n) * np.sin(np.pi * n * f_1 * t)
+    return signal
